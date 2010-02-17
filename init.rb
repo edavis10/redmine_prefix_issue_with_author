4330a38d4f4e9015b0d6fe7d7bddd83132f994bd
Eric Davis <edavis@littlestreamsoftware.com>
2010-02-16 16:09:49 -0800

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_prefix_issue_with_author do
+  name 'Redmine Prefix Issue With Author plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
