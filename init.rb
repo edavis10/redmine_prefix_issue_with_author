611a762cbc0560b62bbd88688b59fa149ba76278
Eric Davis <edavis@littlestreamsoftware.com>
2010-02-16 16:31:30 -0800

    Added plugin registration info

--- init.rb
+++ init.rb
@@ -8,8 +8,13 @@ Dispatcher.to_prepare :redmine_prefix_issue_with_author do
 end
 
 Redmine::Plugin.register :redmine_prefix_issue_with_author do
-  name 'Redmine Prefix Issue With Author plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'Prefix Issue with Author'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
+  author_url 'http://www.littlestreamsoftware.com'
+  description "Redmine plugin that will prefix the Issue subject with the author's name."
+  
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.8.7'
 end
